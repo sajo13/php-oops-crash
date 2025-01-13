dc92c68fe610376c189bc776558e520f961c3365
sajo13 <sajosunny007@gmail.com>
2025-01-13 11:08:37 +0530

    Class in Object Oriented Programming
    - A class is a blueprint or template that defines.
    the properties (attributes) and behaviors (methods) of objects.
    - An object is an instance of a class.

---
+++ 01_classes.php
@@ -0,0 +1,21 @@
+<?php
+
+#Object-oriented programming (OOP) is a programming paradigm that focuses 
+# on the concept of objects. In PHP, you can utilize object-oriented programming 
+#to structure your code in a more organized and modular manner. 
+# Here are the key aspects of PHP object-oriented programming
+
+#Classes and Objects: A class is a blueprint or template that defines 
+# the properties (attributes) and behaviors (methods) of objects. An object is an instance of a class.
+
+class Car {
+    public $color;
+    public function startEngine() {
+        // Code to start the engine
+        echo 'Start Engine function';
+    }
+}
+
+$myCar = new Car();
+$myCar->color = 'blue';        //assign the properties to class.
+$myCar->startEngine();         //call the class function using object.
